befa9b7b5e3580edb51ce63f7cedaf1ee80a6af3
arraken <52732965+arraken@users.noreply.github.com>
2026-03-19 08:11:36 -0500

    Delete private directory

--- private/bot_token.py
+++
@@ -1,8 +0,0 @@
-# Replace these with your actual values before running the bot.
-# UUID will be auto-generated and appended on first run if left blank.
-
-PUBLIC_TOKEN = "YOUR_BOT_TOKEN_HERE"
-BOT_TOKEN
-CHECKSUM_KEY = "5343Savvy!s0d@"
-UUID = ""
-
